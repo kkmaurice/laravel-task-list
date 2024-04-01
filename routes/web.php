ba6cafa44a2510733e59ecbe30c8c73e101c4356
kkmaurice <adonyomaurice@gmail.com>
2024-04-01 08:04:10 +0300

    learnt lavel edit form

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
+
 Route::get('/tasks', function () {
     return view('index', [
         'tasks'=> Task::latest()->get(),
@@ -28,11 +29,17 @@ Route::get('/tasks', function () {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
+Route::get('/tasks/{id}/edit', function ($id) {
+    
+    return view('edit', ['task' => Task::findOrFail($id)]);
+})->name('tasks.edit');
+
 Route::get('/tasks/{id}', function ($id) {
     
     return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
+// create route
 Route::post('/tasks', function(Request $request) {
   $data = $request->validate(
     [
@@ -51,6 +58,27 @@ Route::post('/tasks', function(Request $request) {
 
   return redirect()->route('tasks.show', ['id'=> $task->id])->with('success','Task created successfully!😂😍');
 })->name('tasks.store');
+
+
+// Edit route
+Route::put('/tasks/{id}', function($id, Request $request) {
+  $data = $request->validate(
+    [
+      'title'=> 'required|max:255',
+      'description'=>'required',
+      'long_description'=> 'required',
+    ]
+  );
+
+  $task = Task::findOrFail($id);
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+
+  $task->save();
+
+  return redirect()->route('tasks.show', ['id' => $task->id])->with('success','Task updated successfully!👍😁👌');
+})->name('tasks.update');
 
 
 // Route::get('/xxx', fn() => "hello world home of survival")->name("home");
